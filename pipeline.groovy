d0d0f9681ad3b90be1f0a1e4982f2b93dbe234ac
sdblepas <sdblepas+github@gmail.com>
2021-07-01 15:19:44 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,9 +1,6 @@
 #!/usr/bin/env groovy
 
 import groovy.json.JsonSlurper
-MAXTEMP = $
-
-
 
 pipeline {
 	node {
@@ -27,7 +24,7 @@ pipeline {
 		}
 		stage ('processing'){
 			steps{
-				def task = "python exl_json_to_yml.py".execute() 
+				def task = "python exl_json_to_yml.py".execute() $Operation $Args
 			}
 		}
 		stage ('post-processing'){
@@ -51,7 +48,7 @@ def GetMaxTemp() {
 		def ApiUrl = new URL('https://api.openweathermap.org/data/2.5/onecall?lat=31.76&lon=35.21&exclude=hourly,daily&appid=2e38ebbb02a9b936c8d42f73a94a6dee&units=metric')
 		def JSON = new JsonSlurper().parse(ApiUrl)
 		Float idValue = JSON.get("current").get("temp");
-		if (idValue>MAXTEMP) {
+		if (idValue>$MAXTEMP) {
 			return true
 		}else {
 			return false
